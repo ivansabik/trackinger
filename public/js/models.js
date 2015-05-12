cab35cac2c1bd21257d3a337545c6046edf7d99e
ivan sabik <ivan.rodriguez.o@gmail.com>
2015-05-12 16:57:15 -0500

    models.js, qunit

---
+++ public/js/models.js
@@ -0,0 +1,20 @@
+var Route() {
+  this.id = null;
+  this.status = null;
+  this.startedAt = null;
+  this.geopoints = [];
+  this.getStart() = function() {}
+  this.addGeopoint() = function() {}
+  this.getDistance() = function() {]
+  this.getTime() = function() {}
+  this._generateId() = function() {}
+  this.getMapsPolyline() = function() {}
+  this.getMapsMarkers() = function() {}
+  this.save() = function() {}
+}
+
+var Geopoint() {
+  this.lat = null;
+  this.lon = null;
+  this.timestamp = null;
+}
